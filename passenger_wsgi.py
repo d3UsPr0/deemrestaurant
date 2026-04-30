9434eedec58f94b2f16d58b327f8a952f15778ac
d3uspr0 <deuc60@gmail.com>
2026-04-30 10:19:16 +0300

    Version 1

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,12 +1,11 @@
-import sys
 import os
+import sys
 
-# Get the project directory path
-project_dir = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, project_dir)
+# Path to your Django project (folder containing manage.py)
+sys.path.insert(0, os.path.dirname(__file__))
 
-# Set settings module
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'deemrestaurant.settings')
+# Point to Django settings module
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "deemrestaurant.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
